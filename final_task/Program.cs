8727707d44f19851f7d4e8aafde1e1a77a0f59c6
Dmitriy <potapidze.mos@mail.ru>
2022-11-24 21:23:58 +0300

    Добавил решение задачи

---
+++ final_task/Program.cs
@@ -0,0 +1,61 @@
+﻿//  Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+// Например:
+
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+
+
+Console.WriteLine("Введите значения массива через пробел: ");
+string[] array = Console.ReadLine().Split();
+
+string[] CheckingTheArray(string[] input, int n)
+{
+    string[] output = new string[CountArray(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountArray(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+string[] result = CheckingTheArray(array, 3);
+Console.WriteLine();
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(result);
